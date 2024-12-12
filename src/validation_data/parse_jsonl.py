62ba278734fa12702dd2e3590e61657d93874f4d
tenkalden <tenkalden007@gmail.com>
2024-12-12 14:56:30 +0530

    updated script

--- src/validation_data/parse_jsonl.py
+++ src/validation_data/parse_jsonl.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import json
 import unicodedata
@@ -5,14 +6,13 @@ from urllib.parse import urlparse, urlunparse, ParseResult
 
 
 def parse_image_url(url):
-    """clean imag url"""
     parsed_url = urlparse(url)
     stripped_url = ParseResult(parsed_url.scheme, parsed_url.netloc, parsed_url.path, '', '', '')
     return urlunparse(stripped_url)
 
 
 def get_writing_type(accept_list):
-    """writing type based on the accept list."""
+    """Determine writing type based on the accept list."""
     if 2 in accept_list:
         return "Uchen"
     elif 1 in accept_list:
@@ -41,21 +41,23 @@ def process_jsonl_entry(entry):
     }
 
 
-def convert_jsonl_to_csv(jsonl_file, csv_file):
-    """Convert JSONL file to a CSV file."""
-    with open(jsonl_file, 'r', encoding='utf-8') as infile, open(csv_file, 'w', newline='', encoding='utf-8') as outfile:
+def convert_jsonl_to_csv(jsonl_dir, csv_file):
+    with open(csv_file, 'w', newline='', encoding='utf-8') as outfile:
         fieldnames = ["origin_id", "img_url", "target", "writing_type", "score"]
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
         writer.writeheader()
-
-        for line in infile:
-            entry = json.loads(line)
-            row = process_jsonl_entry(entry)
-            writer.writerow(row)
+        for filename in os.listdir(jsonl_dir):
+            if filename.endswith(".jsonl"):
+                jsonl_file = os.path.join(jsonl_dir, filename)
+                with open(jsonl_file, 'r', encoding='utf-8') as infile:
+                    for line in infile:
+                        entry = json.loads(line)
+                        row = process_jsonl_entry(entry)
+                        writer.writerow(row)
 
 
 if __name__ == "__main__":
-    input_jsonl_file = "data/recheck_b11_to_b16/recheck_ltt_ann_batch11.jsonl"
+    input_jsonl_dir = "data/recheck_b11_to_b18" 
     output_csv_file = "data/output_csv/ocr_validation_data.csv"
-    convert_jsonl_to_csv(input_jsonl_file, output_csv_file)
+    convert_jsonl_to_csv(input_jsonl_dir, output_csv_file)
     print(f"Conversion complete. CSV saved to {output_csv_file}")
